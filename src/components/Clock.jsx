de9b66c618d1deb636b5366e6d4e8cca96343aaa
Rakshit Sain <rakshitsain895@gmail.com>
2024-05-07 18:48:39 -0400

    assignment done

---
+++ src/components/Clock.jsx
@@ -0,0 +1,16 @@
+import UseTime from "../hooks/UseTime";
+
+
+const Clock = () => {
+  const currentTime = UseTime();
+
+
+
+  return (
+    <div>
+      <span>Current Time : {currentTime}</span>
+    </div>
+  );
+};
+
+export default Clock;
---
+++ src/hooks/UseTime.jsx
@@ -0,0 +1,24 @@
+import { useEffect, useRef, useState } from "react";
+
+
+
+function useInterval(callback, delay) {
+  const savedCallback = useRef();
+  useEffect(() => {
+    savedCallback.current = callback;
+  }, [callback]);
+  useEffect(() => {
+    if (delay !== null) {
+      let id = setInterval(() => savedCallback.current(), delay);
+      return () => clearInterval(id);
+    }
+  }, [delay]);
+}
+
+
+
+export default function UseTime() {
+  const [date, setDate] = useState(new Date());
+  useInterval(() => setDate(new Date()), 1000);
+  return date.toLocaleTimeString();
+}
